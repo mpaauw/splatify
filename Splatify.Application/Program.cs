a52613be4f713dda9351bb50bd6aca45312e3635
adjutant-xx <matt.paauw@gmail.com>
2017-01-22 12:38:46 -0800

    Cleaned up Splatify.Application/Program.cs.

--- Splatify.Application/Program.cs
+++ Splatify.Application/Program.cs
@@ -15,23 +15,17 @@ namespace Splatify.Application
         {
             try
             {
-                //...
                 WebAPIService _service = new WebAPIService();
                 PlaylistService _playlistService = new PlaylistService();
                 string _userID = ConfigurationManager.AppSettings[AppSettingKeys.SpotifyUserName.Name].ToString();
-
                 string token = _service.GetAccessToken();
                 string test_playlist = "3LeWNYvsiCCZS0vpEKmeLr";
                 Task<Playlist> playlists = _playlistService.GetPlaylist(token, _userID, test_playlist);
-
-                //_service.TestRestSharp();
             }
             catch (Exception e)
             {
-                //Catch exception here...
+                throw e;
             }
-
-
         }
     }
 }
